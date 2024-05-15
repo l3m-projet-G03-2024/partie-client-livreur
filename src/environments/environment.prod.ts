725552a7d5fb394806de34b21e629bd8e0607ef8
l3miage-couliban <n-djo-soro.coulibaly@etu.univ-grenoble-alpes.fr>
2024-05-15 04:33:46 +0200

    set env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,17 @@
-export const environment = {};
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyCHBnrh6ZebtXGALnfuf4SsHt9Sj0puShk",
+        authDomain: "proj-int-2024-g03.firebaseapp.com",
+        projectId: "proj-int-2024-g03",
+        storageBucket: "proj-int-2024-g03.appspot.com",
+        messagingSenderId: "1034989831553",
+        appId: "1:1034989831553:web:327ecb58750b08dc859480",
+        measurementId: "G-Z9SE3XWX93"
+    },
+    mainServer: "http://129.88.210.152:8080",
+    plannerWsServer: "http://129.88.210.152:4201",
+    openRouteService: {
+        apiKey: "5b3ce3597851110001cf6248c7455426e9964b10aa4943b20d16176b"
+    }
+};
